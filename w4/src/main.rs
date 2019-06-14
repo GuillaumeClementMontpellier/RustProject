0e34c09e5c8d9a61d3e579177dfd0d18f74d8f06
GuillaumeClementPolytech <guillaume.clement@etu.umontpellier.fr>
2019-06-14 18:14:50 +0200

    File de message

--- w4/src/main.rs
+++ w4/src/main.rs
@@ -1,16 +1,46 @@
 use std::thread;
+use std::sync::mpsc;
 use std::time::Duration;
 
-fn main() {
-    thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
+fn main() {let (tx, rx) = mpsc::channel();
 
-    for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
-    }
+	let tx1 = mpsc::Sender::clone(&tx);
+
+	let h1 = thread::spawn(move || {
+		let vals = vec![
+		String::from("hi"),
+		String::from("from"),
+		String::from("the"),
+		String::from("thread"),
+		];
+
+		for val in vals {
+			tx1.send(val).unwrap();
+			thread::sleep(Duration::from_secs(1));
+		}
+	});
+
+	let h2 = thread::spawn(move || {
+		let vals = vec![
+		String::from("more"),
+		String::from("messages"),
+		String::from("for"),
+		String::from("you"),
+		];
+
+		for val in vals {
+			tx.send(val).unwrap();
+			thread::sleep(Duration::from_secs(1));
+		}
+	});
+
+	for received in rx { //bloquant jusqu'a ce que les 2 thread se ferment (les 2 tx sont drop)
+		println!("Got: {}", received);
+	}
+
+	println!("End of Recv");
+
+	h1.join().unwrap();
+	h2.join().unwrap();
+
 }
